09ee97dee0e895d66dfd6e77a4bc2531dee0f045
AbhinavChandurkar <abhinavchandurkar55@gmail.com>
2022-07-22 09:44:14 +0530

    typ fix

--- model/user.model.js
+++ model/user.model.js
@@ -1,5 +1,16 @@
+/**
+ *  This file will contain informatin about the user model
+ */
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    
+    /**
+     *  name,email,password,age,gender,destinatin,depature 
+     * 
+     */
+    name : {
+        type : String,
+        required : true
+    }
 })
